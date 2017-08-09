ebbb55cdf21cbfe732c858811156340784f2c227
Jordan Ta <tajordan88@gmail.com>
2017-08-09 14:35:54 -0400

    Refactor component hero property.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Heroes App';
+  hero: Hero = {
+  	id: 1,
+  	name: 'Stormcloud'
+  };
 }
