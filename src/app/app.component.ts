e3fa8378bf59fbf3cd46dc78eb65e65f48a7a468
Jordan Ta <tajordan88@gmail.com>
2017-08-09 14:45:41 -0400

    Refactor @component to account for new object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class Hero {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 export class AppComponent {
   title = 'Angular Heroes App';
