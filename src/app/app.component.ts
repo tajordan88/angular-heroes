1d14aa407ace90635b4e256efa5053b26afbc31a
Jordan Ta <tajordan88@gmail.com>
2017-08-14 17:32:03 -0400

    Add Hero Array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,3 +26,17 @@ export class AppComponent {
   	name: 'Stormcloud'
   };
 }
+
+
+const HEROES: Hero[] = [
+	{ id: 1, name: 'Bilbo Bagins' },
+	{ id: 2, name: 'Frodo Bagins' },
+	{ id: 3, name: 'Gandolf The Grey' },
+	{ id: 4, name: 'Gandolf The White' },
+	{ id: 5, name: 'Sauron' },
+	{ id: 6, name: 'Legolas' },
+	{ id: 7, name: 'Mary' },
+	{ id: 8, name: 'Pipin' },
+	{ id: 9, name: 'Orc' },
+	{ id: 10, name: 'Elf' }
+];
