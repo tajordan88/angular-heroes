b59f76f5b18fe44cd114f9ad44de497e132c1cb9
Jordan Ta <tajordan88@gmail.com>
2017-10-06 17:01:25 -0700

    Refactor code to split the AppComponent into HeroesComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,21 @@ import { AppComponent } from './app.component';
 
 import { HeroDetailComponent } from './hero-detail.component';
 
+import { HeroesComponent } from './heroes.component';
+
+import { HeroService } from './hero.service';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
